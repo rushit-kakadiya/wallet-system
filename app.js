70631ea66dc8267dab8415cb6364da6a4cdf1b15
Rushit Kakadiya <rushitkakadiya@gmail.com>
2025-03-10 01:43:44 +0530

    update client URL

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: process.env.CLIENT_URL || "https://rushit-wallet-system.netlify.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
